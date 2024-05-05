891ee70276e55211b07cd1a714c3eb201318cc38
aminobot22 <76242458+aminobot22@users.noreply.github.com>
2024-05-05 22:41:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from util import *
 import time
 SLEEP_TIME=10 # minimum
 AUTH_TOKEN="auth token from inspect"
-
+WHITE_LIST_IDS=[1,2,3]
 guil_res=guild_list=get_guild_ids(AUTH_TOKEN) #Provide a list of all the servers that your account is in.
 if guil_res.status_code==200:
 
     for guild_dict in guil_res.json():
         guild_id=guild_dict["id"]
-        print(guild_id)
-        time.sleep(SLEEP_TIME)
-        print(leave_guild(AUTH_TOKEN,guild_id)) #print empty string if working
-    
-
+        if guild_id not in WHITE_LIST_IDS:
+            print(guild_id)
+            time.sleep(SLEEP_TIME)
+            print(leave_guild(AUTH_TOKEN,guild_id)) #print empty string if working
